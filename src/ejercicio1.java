db5caf2b2c59861a654e44cb5b044dcdab5d6338
Jav8-Dev <javiergarrochomunoz@gmail.com>
2025-09-24 08:31:08 +0200

    ¡

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -3,12 +3,11 @@ public class ejercicio1 {
         for (int inicial = 1; inicial < 100; inicial++) {
             if (inicial % 7 == 0) {
                 System.out.println("Tu numero " + inicial + " es multiplo de 7");
-            } else if (inicial % 13 == 0) {
+            }
+            if (inicial % 13 == 0) {
                 System.out.println("Tu numero " + inicial + "es multiplo de 13");
             }
-            if (inicial % 7 == 0 && inicial % 13 == 0) {
-                System.out.println("Tu numero " + inicial + "es multipllo de los dos");
-            }
+
 
         }
     }
---
+++ src/ejercicio2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner jose =  new Scanner(System.in);
+        System.out.println("Introduce un numero del 1 al 10");
+    }
+}
