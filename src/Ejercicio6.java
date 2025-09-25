939fb86ed3d07238f3be448626345bba1e88f4ac
Jav8-Dev <javiergarrochomunoz@gmail.com>
2025-09-25 13:36:56 +0200

    de el 6 si estoy orgulloso

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Ejercicio6 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Te voy a pedir dos numeros tontorron");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int operacion =  0;
+        for (int i = 0; i < num2 ; i++) {
+            operacion += num1;
+        }
+        System.out.println("El total ha sido de " + operacion);
+        }
     }
-}
+
